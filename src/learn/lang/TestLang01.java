06446cab1b260c3a73a1c1e39f3f5d085974ffb1
wangzhi <wang.zhi@intellif.com>
2020-03-31 20:24:31 +0800

    初始化jdk8源码调试环境

---
+++ src/learn/lang/TestLang01.java
@@ -0,0 +1,13 @@
+package learn.lang;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestLang01 {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("data", "aa");
+    }
+}
